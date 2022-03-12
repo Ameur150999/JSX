215c932c191395ab9224472f101e06e7aba93c86
Ameur Saoudi <saoudiameur0@gmail.com>
2022-03-12 12:09:31 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
+import React, { Component } from "react";
+import Ameur from './Ameur.JPG';
+import { Player } from 'video-react';
 import './App.css';
-
+import './style.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1 class="titlered">Ameur Saoudi</h1>
+    <br/> <br/>
+    <h2>Public Picture</h2>
+    <img src='Foot.JPG' alt='pic' height={500} width={800}/>
+    <br/> <br/>
+    <h2>SRC Picture</h2>
+    <img src={Ameur} alt ='myImage' height={800} width={500}/>
+    <br/> <br/>
+    <h2>Video</h2>
+    <video width="750" height="500" controls >
+      <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" type="video/mp4"/>
+     </video>
+    </>
+ 
   );
 }
 
